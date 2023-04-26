f359551095ecff2ac9718a529a18bf19d3e21872
AviralPatel <aviralpatel12379@icloud.com>
2023-04-26 13:18:07 +0530

    Create selectionSort.c

---
+++ selectionSort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        // Swap the found minimum element with the element at index i
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void print_array(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    print_array(arr, n);
+
+    selection_sort(arr, n);
+
+    printf("Sorted array:\n");
+    print_array(arr, n);
+
+    return 0;
+}
